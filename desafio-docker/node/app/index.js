0ac10015f3319118414a5e66d68a865b461d776b
Daniel Nunes <danielmnunes08@gmail.com>
2021-03-05 22:20:00 -0300

    desafio docker com node

---
+++ desafio-docker/node/app/index.js
@@ -0,0 +1,35 @@
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+let sql1 = "CREATE TABLE IF NOT EXISTS people (id integer not null auto_increment primary key, name varchar(30));"
+connection.query(sql1)
+
+const express = require('express');
+const app = express();
+
+app.get('/:nome', function (req, res) {
+  const sql = `INSERT INTO people(name) values('${req.params.nome}')`
+  connection.query(sql)
+  connection.query('SELECT * FROM people', (err, rows, fields) => {
+    resposta = `<h1>Full Cycle Rocks!</h1>
+        <ul>`
+    if (!err) {
+      for (i = 0; i < rows.length; i++) {
+        resposta += `<li>${rows[i].name}</li>`
+      }
+      resposta = resposta + `</ul>`
+    } else
+      resposta = "erro na consulta"
+
+    res.send(resposta)
+  });
+});
+
+app.listen(5000, () => console.log('Server is up and running'));
+
